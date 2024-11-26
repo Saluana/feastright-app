374ec138bcff14d321e66c9d32c5a0a199fd10b0
alexy-os <agorbunoff.ru@gmail.com>
2024-11-26 15:11:34 +0300

    optimize

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     postcss: {
       plugins: [tailwind(), autoprefixer()],
     },
+    devSourcemap: false,
   },
   plugins: [vue()],
   resolve: {
@@ -17,12 +18,51 @@ export default defineConfig({
     }
   },
   build: {
+    target: 'esnext',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info'],
+        passes: 2
+      },
+      format: {
+        comments: false,
+        preserve_annotations: false
+      },
+      mangle: {
+        toplevel: true
+      }
+    },
     rollupOptions: {
       output: {
-        manualChunks: {
-          'sheet': ['@/components/ui/sheet']
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) {
+              return 'vue-core';
+            }
+            if (id.includes('@vueuse')) {
+              return 'vue-use';
+            }
+            if (id.includes('radix-vue')) {
+              return 'radix';
+            }
+            return 'vendors';
+          }
         }
       }
+    },
+    assetsInlineLimit: 4096,
+    sourcemap: false,
+    cssCodeSplit: true,
+    modulePreload: {
+      polyfill: true
+    }
+  },
+  server: {
+    hmr: {
+      overlay: false
     }
   }
 })
