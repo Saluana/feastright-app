bd5ae2333130297d2fb7173ca360ee53f5c4f79e
Saluana <46250649+Saluana@users.noreply.github.com>
2025-05-17 18:22:38 -0700

    Implement code changes to enhance functionality and improve performance

--- src/composables/useState.ts
+++ src/composables/useState.ts
@@ -1,7 +1,7 @@
 import { ref } from "vue"
 
 const isOnline = ref(true)
-const isProduction = ref(true)
+const isProduction = ref(false)
 const backendDevelopmentHost = ref('http://localhost:4200')
 const backendProductionHost = ref('https://api.feastright.com')
 const host = ref(isProduction.value ? backendProductionHost.value : backendDevelopmentHost.value)
